947951e40da25e79a6390174ab38693fd85de0a3
Victor <arreagav17@gmail.com>
2023-06-24 12:55:06 -0500

    Refactor

--- config.h
+++ config.h
@@ -82,19 +82,14 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-/* commands */
-static const char *dmenucmd[] = { "dmenu-run-i", NULL };
-static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	{ MODKEY,                           XK_c,       spawn,          SHCMD("rofi -show drun") },
@@ -106,20 +101,20 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,                 XK_m,       spawn,          SHCMD("microphone toggle")},
 
     // toggle stuff
-    { MODKEY,                           XK_b,       togglebar,      {0} },
-    { MODKEY|ControlMask,               XK_t,       togglegaps,     {0} },
-    { MODKEY|ShiftMask,                 XK_space,   togglefloating, {0} },
-    { MODKEY,                           XK_f,       togglefullscr,  {0} },
+    // { MODKEY,                           XK_b,       togglebar,      {0} },
+    // { MODKEY|ControlMask,               XK_t,       togglegaps,     {0} },
+    // { MODKEY|ShiftMask,                 XK_space,   togglefloating, {0} },
+    // { MODKEY,                           XK_f,       togglefullscr,  {0} },
 
-    { MODKEY|ControlMask,               XK_w,       tabmode,        { -1 } },
+    // { MODKEY|ControlMask,               XK_w,       tabmode,        { -1 } },
     { MODKEY,                           XK_j,       focusstack,     {.i = +1 } },
     { MODKEY,                           XK_k,       focusstack,     {.i = -1 } },
     { MODKEY,                           XK_i,       incnmaster,     {.i = +1 } },
     { MODKEY,                           XK_d,       incnmaster,     {.i = -1 } },
 
 
-    // { MODKEY,                           XK_h,       setmfact,       {.f = -0.05} },
-    // { MODKEY,                           XK_l,       setmfact,       {.f = +0.05} },
+    { MODKEY,                           XK_h,       setmfact,       {.f = -0.05} },
+    { MODKEY,                           XK_l,       setmfact,       {.f = +0.05} },
     // { MODKEY|ShiftMask,                 XK_h,       setcfact,       {.f = +0.25} },
     // { MODKEY|ShiftMask,                 XK_l,       setcfact,       {.f = -0.25} },
     // { MODKEY|ShiftMask,                 XK_o,       setcfact,       {.f =  0.00} },
@@ -130,8 +125,8 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,                 XK_Return,  zoom,           {0} },
     { MODKEY,                           XK_Tab,     view,           {0} },
     { MODKEY,                           XK_space,   setlayout,      {0} },
-    { MODKEY|ControlMask,               XK_comma,   cyclelayout,    {.i = -1 } },
-    { MODKEY|ControlMask,               XK_period,  cyclelayout,    {.i = +1 } },
+    // { MODKEY|ControlMask,               XK_comma,   cyclelayout,    {.i = -1 } },
+    // { MODKEY|ControlMask,               XK_period,  cyclelayout,    {.i = +1 } },
     { MODKEY,                           XK_0,       view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,                 XK_0,       tag,            {.ui = ~0 } },
     { MODKEY,                           XK_comma,   focusmon,       {.i = -1 } },
@@ -160,8 +155,8 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkStatusText,        0,              Button3,        spawn,          {.v = dmenucmd } },
+	// { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	// { ClkStatusText,        0,              Button3,        spawn,          {.v = dmenucmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
