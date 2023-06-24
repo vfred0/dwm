f4c65fbfb1998960a8872778bfa49b37c7127868
Victor <arreagav17@gmail.com>
2023-06-24 17:32:00 -0500

    Added shortcuts

--- config.h
+++ config.h
@@ -93,8 +93,7 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
-/* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -112,24 +111,16 @@ static const Key keys[] = {
     {NULL,                              XK_Print,   spawn,          SHCMD("flameshot gui")},
 	{ MODKEY|ShiftMask,                 XK_m,       spawn,          SHCMD("microphone toggle")},
 
-    // toggle stuff
-    // { MODKEY,                           XK_b,       togglebar,      {0} },
-    // { MODKEY|ControlMask,               XK_t,       togglegaps,     {0} },
-    // { MODKEY|ShiftMask,                 XK_space,   togglefloating, {0} },
-    // { MODKEY,                           XK_f,       togglefullscr,  {0} },
-
-    // { MODKEY|ControlMask,               XK_w,       tabmode,        { -1 } },
     { MODKEY,                           XK_j,       focusstack,     {.i = +1 } },
     { MODKEY,                           XK_k,       focusstack,     {.i = -1 } },
     { MODKEY,                           XK_i,       incnmaster,     {.i = +1 } },
-    { MODKEY,                           XK_d,       incnmaster,     {.i = -1 } },
-
+    { MODKEY,                           XK_o,       incnmaster,     {.i = -1 } },
 
     { MODKEY,                           XK_h,       setmfact,       {.f = -0.05} },
     { MODKEY,                           XK_l,       setmfact,       {.f = +0.05} },
-    // { MODKEY|ShiftMask,                 XK_h,       setcfact,       {.f = +0.25} },
-    // { MODKEY|ShiftMask,                 XK_l,       setcfact,       {.f = -0.25} },
-    // { MODKEY|ShiftMask,                 XK_o,       setcfact,       {.f =  0.00} },
+    { MODKEY|ShiftMask,                 XK_h,       setcfact,       {.f = +0.25} },
+    { MODKEY|ShiftMask,                 XK_l,       setcfact,       {.f = -0.25} },
+    { MODKEY|ShiftMask,                 XK_o,       setcfact,       {.f =  0.00} },
 
 
     { MODKEY|ShiftMask,                 XK_j,       movestack,      {.i = +1 } },
@@ -137,8 +128,7 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,                 XK_Return,  zoom,           {0} },
     { MODKEY,                           XK_Tab,     view,           {0} },
     { MODKEY,                           XK_space,   setlayout,      {0} },
-    // { MODKEY|ControlMask,               XK_comma,   cyclelayout,    {.i = -1 } },
-    // { MODKEY|ControlMask,               XK_period,  cyclelayout,    {.i = +1 } },
+
     { MODKEY,                           XK_0,       view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,                 XK_0,       tag,            {.ui = ~0 } },
     { MODKEY,                           XK_comma,   focusmon,       {.i = -1 } },
@@ -147,9 +137,7 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,                 XK_period,  tagmon,         {.i = +1 } },
 
 
-    { MODKEY,                           XK_q,       killclient,     {0} },
-
-    { MODKEY|ShiftMask,                 XK_r,       restart,        {1} },
+    { MODKEY,                           XK_q,       killclient,     {0} },    
 	{ MODKEY|ShiftMask,             	XK_q,    	quit,           {0} },
     { MODKEY,                           XK_e,       hidewin,        {0} },
     { MODKEY|ShiftMask,                 XK_e,       restorewin,     {0} },
