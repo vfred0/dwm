7d2a9a8b79e1c6fbadf353d828439f088f344531
Victor <arreagav17@gmail.com>
2023-06-24 13:33:56 -0500

    Added color for layout

--- dwm.c
+++ dwm.c
@@ -830,7 +830,7 @@ drawbar(Monitor *m)
 		x += w;
 	}
 	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
+	drw_setscheme(drw, scheme[SchemeLayout]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
